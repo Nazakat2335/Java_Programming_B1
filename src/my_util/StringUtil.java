b2c05fe7a1fea3c25f4008ab0bdb389eec51a7f8
Nazakat2335 <naza.beybutova.89@gmail.com>
2023-04-02 13:24:35 -0500

    day_24_office_hours_2

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -1,5 +1,22 @@
 package my_util;
 
 public class StringUtil {
+    /**
+     This method made by Loopcamp Batch#1
+     The method accepts a String and returns the reversed String
+     */
+    public static String reverseStr (String str) {
 
+        String reversed = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+    public static void main(String[] args) {
+
+        StringUtil.reverseStr("loopcamp");
+        System.out.println(StringUtil.reverseStr("Hello World!"));
+
+    }
 }
