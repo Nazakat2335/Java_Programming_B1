0dba005965187ba165cd58f5feb5bdeaa1cbec34
Nazakat2335 <naza.beybutova.89@gmail.com>
2023-04-26 21:09:58 -0500

    day34_working

---
+++ src/day34_a_static/School.java
@@ -0,0 +1,8 @@
+package day34_a_static;
+
+public class School {
+    //create an object that has to parameteres:name, groupNum
+    LoopAcademyStudent student1=new LoopAcademyStudent("Javid",1);
+    LoopAcademyStudent student2=new LoopAcademyStudent("Natalia",2);
+
+}
---
+++ src/day34_c_inheritance/animal/Lion.java
@@ -0,0 +1,7 @@
+package day34_c_inheritance.animal;
+
+public class Lion {
+    public void roar(){
+        System.out.println("Roar      ");
+    }
+}
